1f48e486693473043b2dc26d941d6452dfdc24d8
Blake S. Danziger 22 <bdanziger@flume.cs.dartmouth.edu>
2020-05-30 17:19:11 -0400

    init solver files

---
+++ solver/solver.c
@@ -0,0 +1,10 @@
+/*  solver.c file for C-SarSudoku
+ *
+ *  contains functions that are used to solve sudoku puzzles
+ *  see solver.h for more information
+ *
+ *  Blake Danziger, Tracey Mills, Tadeas Uhlir
+ *  CS 50, Spring 2020
+ */
+
+
---
+++ solver/solver.h
@@ -0,0 +1,10 @@
+/*  solver.c file for C-SarSudoku
+ *
+ *  contains functions that are used to solve sudoku puzzles
+ *
+ *  Blake Danziger, Tracey Mills, Tadeas Uhlir
+ *  CS 50, Spring 2020
+ */
+
+
+ 
