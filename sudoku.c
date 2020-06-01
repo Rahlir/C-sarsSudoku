9e498949e43ae7320eb5973238f67d8c3f7c2b84
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2020-06-01 16:40:44 +0200

    Change indentation to 4 spaces

--- sudoku.c
+++ sudoku.c
@@ -13,55 +13,56 @@ void print_grid(int *grid, int size);
 bool process_input(int *grid, int size);
 
 int main(int argc, char *argv[]) {
-	char *mode;
-	int size = 81;
+    char *mode;
+    int size = 81;
 
-	//check arg #
-	if(argc != 2) {
-		fprintf(stderr, "Usage: provide one arg indicating mode\n");
-		return 1;
-	}
-	
-	//allocate memory for grid
-	int *grid = malloc(sizeof(int)*size);
-	if(grid == NULL) {
-		fprintf(stderr, "Error: unable to allocate memory for grid\n");
-		return 2;
-	}
-		
-	mode = argv[1];
-	
-	//create mode
-	if(strcmp(mode, "create") == 0) {
-		//initialize empty grid
-		for(int i=0; i<size; i++)
-			*(grid+i) = 0;
-		//initialize creator
-		creator(grid, size);
-	}
-	//solve mode
-	else if(strcmp(mode, "solve") == 0) {
-		//get puzzle to be solved
-		if(process_input(grid, size)) {
-			//initialize solver
-			solver(grid, size);
-		}
-		else {
-			printf("Error: invalid input\n");
-			return 4;
-		}
-	}
-	//invalid mode
-	else {
-		fprintf(stderr, "Error: mode must be either 'create' or 'solve'\n");
-		free(grid);
-		return 3;
-	}
+    //check arg #
+    if(argc != 2) {
+        fprintf(stderr, "Usage: provide one arg indicating mode\n");
+        return 1;
+    }
 
-	//print output
-	print_grid(grid, size);
-	free(grid);
-	return 0;
+    //allocate memory for grid
+    int *grid = malloc(sizeof(int)*size);
+    if(grid == NULL) {
+        fprintf(stderr, "Error: unable to allocate memory for grid\n");
+        return 2;
+    }
+
+    mode = argv[1];
+
+    //create mode
+    if(strcmp(mode, "create") == 0) {
+        //initialize empty grid
+        for(int i=0; i<size; i++) {
+            *(grid+i) = 0;
+        }
+        //initialize creator
+        creator(grid, size);
+    }
+    //solve mode
+    else if(strcmp(mode, "solve") == 0) {
+        //get puzzle to be solved
+        if(process_input(grid, size)) {
+            //initialize solver
+            solver(grid, size);
+        }
+        else {
+            printf("Error: invalid input\n");
+            return 4;
+        }
+    }
+    //invalid mode
+    else {
+        fprintf(stderr, "Error: mode must be either 'create' or 'solve'\n");
+        free(grid);
+        return 3;
+    }
+
+    //print output
+    print_grid(grid, size);
+    free(grid);
+    return 0;
 }
 
 /*
@@ -70,11 +71,12 @@ int main(int argc, char *argv[]) {
  * returns true if n = "size" ints read into grid, false otherwise
  */
 bool process_input(int *grid, int size) {
-	for(int i=0; i<size; i++) {
-		if(fscanf(stdin, "%d", (grid+i)) != 1)
-		       return false;	
-	}
-	return true;
+    for(int i=0; i<size; i++) {
+        if(fscanf(stdin, "%d", (grid+i)) != 1) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
@@ -86,12 +88,12 @@ bool process_input(int *grid, int size) {
  * Numbers are separated by a whitespace.
  */
 void print_grid(int *grid, int size) {
-	if(size == 81) {
-		for(int i=0; i<9; i++) {
-			for(int j=0; j<9; j++) {
-				printf("%d ", *(grid+(i*9)+j));
-			}
-			printf("\n");
-		}
-	}
+    if(size == 81) {
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                printf("%d ", *(grid+(i*9)+j));
+            }
+            printf("\n");
+        }
+    }
 }
