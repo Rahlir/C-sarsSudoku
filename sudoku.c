be970ba976a2d3f22f2aa11a42be574c88d2e73e
Blake S. Danziger 22 <bdanziger@moose.cs.dartmouth.edu>
2020-06-02 17:11:36 -0400

    undid changes to process_input

--- sudoku.c
+++ sudoku.c
@@ -96,29 +96,8 @@ int main(int argc, char *argv[]) {
  */
 bool process_input(int *grid, int size) {
     for(int i=0; i<size; i++) {
-
-        #ifdef UNITTEST
-            printf("\ni: %d\n", i);
-        #endif
-
-        char ch[2];
-        ch[1] = '?';
-        if(fscanf(stdin, "%c", &ch[0]) == 1) {
-
-            #ifdef UNITTEST
-                printf("ch: %c\n", ch[0]);
-            #endif
-
-            if (isdigit(ch[0])) {
-                sscanf(ch, "%d", (grid+i));
-                
-                #ifdef UNITTEST
-                    printf("grid: %d\n", *(grid+i));
-                #endif
-            }
-            else {
-                i--;
-            }
+        if(fscanf(stdin, "%d", (grid+i)) != 1) {	
+            return false;
         }
     }
     return true;
