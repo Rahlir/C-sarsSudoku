4cd6940ba3f4008b57c26eb91ce96e5a04e680ca
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2020-06-02 14:15:30 +0200

    Fix testing.sh

--- testing.sh
+++ testing.sh
@@ -4,6 +4,8 @@
 #
 # Blake Danziger, Tadeas Uhlir, Tracey Mills
 
+testDir=testfiles
+
 echo "Testing sudoku module"
 echo ""
 #Error cases
@@ -15,32 +17,32 @@ echo "Too many arguments..."
 echo "Invalid arg..."
 ./sudoku xyz
 echo "Solving grid with too few ints:"
-cat error_grid1
-cat error_grid1 | ./sudoku solve
+cat "$testDir"/error_grid1
+cat "$testDir"/error_grid1 | ./sudoku solve
 echo "Solving grid with non-int chars:"
-cat error_grid2
-cat error_grid2 | ./sudoku solve
+cat "$testDir"/error_grid2
+cat "$testDir"/error_grid2 | ./sudoku solve
 
 ##### THIS ERROR NOT CORRECT: IT STARTS READING FROM STANDARD INPUT
 echo "Solving grid with out-of-range ints:"
-cat error_grid3
-cat error_grid3 | ./sudoku solve
+cat "$testDir"/error_grid3
+cat "$testDir"/error_grid3 | ./sudoku solve
 #####
 echo "Solving grid with no solution:"
-cat error_grid4
-cat error_grid4 | ./sudoku solve
+cat "$testDir"/error_grid4
+cat "$testDir"/error_grid4 | ./sudoku solve
 echo "Solving pre-filled grid with no solution:"
-cat error_grid5
-cat error_grid5 | ./sudoku solve
+cat "$testDir"/error_grid5
+cat "$testDir"/error_grid5 | ./sudoku solve
 echo ""
 #Success cases
 echo "These tests should succeed:"
 echo "Solving valid pre-filled grid:"
-cat grid6
-cat grid6 | ./sudoku solve
+cat "$testDir"/grid6
+cat "$testDir"/grid6 | ./sudoku solve
 echo "Solving grid with more than one solution:"
-cat grid7
-cat grid7 | ./sudoku solve
+cat "$testDir"/grid7
+cat "$testDir"/grid7 | ./sudoku solve
 echo "Fuzz testing with 10 randomly generated valid grids:"
 for i in {1..10}
 do
