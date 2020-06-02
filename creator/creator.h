a06b1e5a2333dfafde71070a974a6ba1b63cc780
Tracey Mills <tracey.e.mills.22@dartmouth.edu>
2020-06-01 22:06:32 -0400

    edited comments

--- creator/creator.h
+++ creator/creator.h
@@ -1,17 +1,32 @@
-/*
- * creator - creates sudoku puzzles to be solved
+/*  creator.c file for C-SarSudoku
+ *
+ *  contains functions that are used to create sudoku puzzles
+ *
+ *  Blake Danziger, Tracey Mills, Tadeas Uhlir
+ *  CS 50, Spring 2020
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-//creates grid
+///////////// creator ////////////////
+/* recieves grid to be filled, along with its size
+ * calls other functions to fill grid to make a uniqely solvable sudoku puzzle
+ */
 void creator(int *grid, int size);
 
-//recursively fills valid sudoku grid, returns 1 on success and 0 on failure
+////////////// fill_grid ///////////////
+/* recieves grid to be filled, current field number to fill, and size of grid
+ * recursively fills full valid sudoku grid using back-tracking
+ * returns 1 on success and 0 on failue
+ */
 int fill_grid(int *grid, int field, int size);
 
+////////////// remove_fields  ///////////////
+
+////////////// get_random  ///////////////
 //returns random int min-max inclusive
 int get_random(int min, int max);
